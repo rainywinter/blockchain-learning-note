876f82d896d79912a4ba8a144f560a59a39ffff7
rainywinter <sunny_up_me@163.com>
2017-10-29 10:17:07 +0800

    add field:nonce in block.

--- blockchain/block.go
+++ blockchain/block.go
@@ -1,9 +1,7 @@
 package blockchain
 
 import (
-	"bytes"
-	"crypto/sha256"
-	"strconv"
+	"fmt"
 	"time"
 )
 
@@ -12,18 +10,30 @@ type Block struct {
 	Data          []byte
 	PrevBlockHash []byte
 	Hash          []byte
+	// 为计算合适的hash值
+	Nonce int
 }
 
-func (b *Block) SetHash() {
-	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
-	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
-	hash := sha256.Sum256(headers)
+// 替代为创建新区块时查找合适的哈希值
+// func (b *Block) SetHash() {
+// 	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
+// 	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
+// 	hash := sha256.Sum256(headers)
 
-	b.Hash = hash[:]
-}
+// 	b.Hash = hash[:]
+// }
 
 func NewBlock(data string, prevBlockHash []byte) *Block {
-	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}}
-	block.SetHash()
+	defer func(begin time.Time) {
+		fmt.Printf("Create block,data: %s, cost time: %.2f s.\n", data, time.Now().Sub(begin).Seconds())
+	}(time.Now())
+	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}, 0}
+
+	pow := NewProofOfWork(block)
+	nonce, hash := pow.Run()
+
+	block.Hash = hash
+	block.Nonce = nonce
+
 	return block
 }
